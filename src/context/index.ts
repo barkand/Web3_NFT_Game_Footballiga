75cf052c41aed1176d217ac0262c82cef4a8d902
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-26 11:53:59 +0330

    Move DefaultContext to Public Folder

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,3 +1,3 @@
 export { default as DefaultCulture } from "./default/culture";
 export { default as DefaultTheme } from "./default/theme";
-export { PublicContext } from "./public/public";
+export { PublicContext, PublicProvider } from "./public/public";
