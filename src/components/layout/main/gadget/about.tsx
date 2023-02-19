8a26c3247cdba439502475d6ec86515d888929be
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-02-20 01:40:40 +0330

    Fix Padding for Mobile

--- src/components/layout/main/gadget/about.tsx
+++ src/components/layout/main/gadget/about.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { useTranslation } from "gatsby-plugin-react-i18next";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 import { Label } from "../../../";
 
 export default function About() {
   const { t } = useTranslation();
+  const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down("md"));
 
   return (
     <div
-      style={{
-        paddingBottom: 70,
-        paddingLeft: 200,
-        paddingRight: 200,
-      }}
+      style={
+        isMobile
+          ? {
+              paddingBottom: 70,
+              paddingLeft: 20,
+              paddingRight: 20,
+            }
+          : { paddingBottom: 70, paddingLeft: 200, paddingRight: 200 }
+      }
     >
       <Label size="h6" sx={{ pb: 2 }}>
         {t("about1")}
